105af6775d5c4ef28fb4819076e6c1570e074842
ulrkle <ulrich.kleeberger@witt-gruppe.eu>
2017-08-30 09:21:03 +0200

    Polish

--- src/main/java/com/example/RefreshApplication.java
+++ src/main/java/com/example/RefreshApplication.java
@@ -3,11 +3,9 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableZuulProxy
 public class RefreshApplication {
 
 	public static void main(String[] args) {
